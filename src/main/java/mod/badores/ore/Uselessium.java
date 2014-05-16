b63b0f6239b8ecbdfc67af4b34ddc25506b2856d
diesieben07 <diesieben07@gmail.com>
2014-05-16 15:20:03 +0200

    make uselessium smeltable

--- src/main/java/mod/badores/ore/Uselessium.java
+++ src/main/java/mod/badores/ore/Uselessium.java
@@ -6,6 +6,11 @@ package mod.badores.ore;
 public class Uselessium extends AbstractOre {
 
 	@Override
+	public boolean hasIngot() {
+		return true;
+	}
+
+	@Override
 	public String getName() {
 		return "uselessium";
 	}
