0e85621a8c1f30e6e168dab0668bd5b8c90e1638
diesieben07 <diesieben07@gmail.com>
2014-05-17 15:10:51 +0200

    Rename EntityAccessor for unique name Make Fleesonsite play sound directly

--- src/main/java/net/minecraft/entity/BadOresEntityAccessor.java
+++ src/main/java/net/minecraft/entity/BadOresEntityAccessor.java
@@ -3,7 +3,7 @@ package net.minecraft.entity;
 /**
  * Created by Lukas Tenbrink on 16.05.2014.
  */
-public class EntityAccessor {
+public class BadOresEntityAccessor {
     public static String getLivingSound(EntityLiving entityLiving)
     {
         return entityLiving.getLivingSound();
