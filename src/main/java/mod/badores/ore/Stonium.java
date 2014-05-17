07285a9f45d25dd68676424a68484cf6e0be9b35
diesieben07 <diesieben07@gmail.com>
2014-05-17 18:38:20 +0200

    make stonium generate in a 2 high block layer

--- src/main/java/mod/badores/ore/Stonium.java
+++ src/main/java/mod/badores/ore/Stonium.java
@@ -1,8 +1,12 @@
 package mod.badores.ore;
 
+import mod.badores.BadOres;
+import mod.badores.oremanagement.BlockInfo;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.IChunkProvider;
 
 import java.util.List;
 import java.util.Random;
@@ -40,5 +44,25 @@ public class Stonium extends AbstractOre {
     @Override
 	public void getDroppedItems(World world, int x, int y, int z, int meta, int fortune, List<ItemStack> drops) {
 		drops.add(new ItemStack(Blocks.stone));
+	}
+
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+		BlockInfo blockInfo = BadOres.oreManager.getBlockInfo(this);
+		Chunk chunk = world.getChunkFromChunkCoords(chunkX, chunkZ);
+
+		int bX = chunkX * 16;
+		int bZ = chunkZ * 16;
+		int bXE = bX + 16;
+		int bZE = bZ + 16;
+		for (int y = 45; y < 48; ++y) {
+			for (int x = 0; x < 16; ++x) {
+				for (int z = 0; z < 16; ++z) {
+					if (rand.nextInt(2) == 0 && chunk.getBlock(x, y, z) == Blocks.stone) {
+						chunk.func_150807_a(x, y, z, blockInfo.block, blockInfo.metadata);
+					}
+				}
+			}
+		}
 	}
 }
