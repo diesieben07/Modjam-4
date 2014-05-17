60114a2a19f2d1a64a57d28ae068f19af4a45a94
diesieben07 <diesieben07@gmail.com>
2014-05-17 17:25:58 +0200

    make fleesonsite "flee" in your inventory

--- src/main/java/mod/badores/ore/Fleesonsite.java
+++ src/main/java/mod/badores/ore/Fleesonsite.java
@@ -3,7 +3,9 @@ package mod.badores.ore;
 import cpw.mods.fml.relauncher.Side;
 import mod.badores.entities.EntityFleeingBlock;
 import mod.badores.oremanagement.BlockInfo;
+import mod.badores.oremanagement.OreForm;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 import java.util.Random;
@@ -31,6 +33,22 @@ public class Fleesonsite extends AbstractOre {
             fleeingBlock.setTarget(player);
             world.spawnEntityInWorld(fleeingBlock);
 			fleeingBlock.playLivingSound();
+		}
+	}
+
+	@Override
+	public void onInventoryTick(OreForm form, ItemStack stack, int slot, EntityPlayer player, World world, Side side) {
+		if (side.isServer() && rand.nextInt(150) == 0) {
+			int targetSlot;
+			int count = 0;
+			do {
+				targetSlot = rand.nextInt(player.inventory.mainInventory.length);
+				if (++count == 15) {
+					return;
+				}
+			} while (player.inventory.getStackInSlot(targetSlot) != null);
+			player.inventory.setInventorySlotContents(targetSlot, stack);
+			player.inventory.setInventorySlotContents(slot, null);
 		}
 	}
 
