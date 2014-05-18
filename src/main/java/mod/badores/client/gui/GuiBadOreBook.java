a56bdc643246f5e1594d24739e80495aadd439bf
diesieben07 <diesieben07@gmail.com>
2014-05-18 14:27:51 +0200

    added change page button

--- src/main/java/mod/badores/client/gui/GuiBadOreBook.java
+++ src/main/java/mod/badores/client/gui/GuiBadOreBook.java
@@ -1,9 +1,26 @@
 package mod.badores.client.gui;
 
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.ResourceLocation;
 
 /**
  * @author diesieben07
  */
 public class GuiBadOreBook extends GuiScreen {
+
+	static final ResourceLocation texture = new ResourceLocation("minecraft:textures/gui/book.png");
+	private int page = 0;
+
+	@Override
+	public void drawScreen(int mouseX, int mouseY, float renderPartials) {
+		super.drawScreen(mouseX, mouseY, renderPartials);
+
+		mc.renderEngine.bindTexture(texture);
+		drawTexturedModalRect((width - 192) / 2, 2, 0, 0, 192, 192);
+	}
+
+	@Override
+	public boolean doesGuiPauseGame() {
+		return false;
+	}
 }
---
+++ src/main/java/mod/badores/client/gui/GuiButtonChangePage.java
@@ -0,0 +1,40 @@
+package mod.badores.client.gui;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+
+import static org.lwjgl.opengl.GL11.glColor4f;
+
+/**
+ * @author diesieben07
+ */
+public class GuiButtonChangePage extends GuiButton {
+
+	private final boolean previous;
+
+	public GuiButtonChangePage(int id, int x, int y, boolean previous) {
+		super(id, x, y, 23, 13, "");
+		this.previous = previous;
+	}
+
+	@Override
+	public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+		if (this.visible) {
+			boolean mouseOver = mouseX >= xPosition && mouseY >= yPosition && mouseX < xPosition + width && mouseY < yPosition + height;
+			glColor4f(1, 1, 1, 1);
+			mc.renderEngine.bindTexture(GuiBadOreBook.texture);
+			int u = 0;
+			int v = 192;
+
+			if (mouseOver) {
+				u += 23;
+			}
+
+			if (previous) {
+				v += 13;
+			}
+
+			drawTexturedModalRect(xPosition, yPosition, u, v, 23, 13);
+		}
+	}
+}
