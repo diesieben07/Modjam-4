5a2999226e9c9084325fb9d56afef9ae9da7c3a4
Lukas Tenbrink <lukastenbrink@googlemail.com>
2014-05-19 00:05:08 +0200

    Add killedNosleeptonite achievement

--- src/main/java/mod/badores/achievements/BOAchievementList.java
+++ src/main/java/mod/badores/achievements/BOAchievementList.java
@@ -15,7 +15,8 @@ public class BOAchievementList {
 
     public static Achievement barelyGeneriteFound;
     public static Achievement buildBarelyGeneriteBlock;
-	public static Achievement iwontfiteDamage;
+    public static Achievement iwontfiteDamage;
+    public static Achievement killedNosleeptonite;
 
     private static ItemStack getOreStack(String oreID) {
         return BadOres.oreManager.getBlockInfo(BadOres.oreManager.getOreByName(oreID)).asStack();
@@ -28,9 +29,10 @@ public class BOAchievementList {
     public static void init() {
         barelyGeneriteFound = (new Achievement("achievement.barelyGeneriteFound", "barelyGeneriteFound", 0, 2, getOreStack("barelyGenerite"), null)).registerStat();
         buildBarelyGeneriteBlock = new Achievement("achievement.buildBarelyGeneriteBlock", "buildBarelyGeneriteBlock", 3, 2, getOreBlockStack("barelyGenerite"), barelyGeneriteFound).registerStat();
-	    iwontfiteDamage = new Achievement("achievement.iwontfiteDamage", "iwontfiteDamage", 0, 0, getOreStack("iwontfite"), null).registerStat();
+        iwontfiteDamage = new Achievement("achievement.iwontfiteDamage", "iwontfiteDamage", 0, 0, getOreStack("iwontfite"), null).registerStat();
+        killedNosleeptonite = new Achievement("achievement.killedNosleeptonite", "killedNosleeptonite", 2, -2, getOreStack("nosleeptonite"), null).registerStat();
 
-	    page = new AchievementPage(BadOres.NAME, barelyGeneriteFound, buildBarelyGeneriteBlock, iwontfiteDamage);
+	    page = new AchievementPage(BadOres.NAME, barelyGeneriteFound, buildBarelyGeneriteBlock, iwontfiteDamage, killedNosleeptonite);
 	    AchievementPage.registerAchievementPage(page);
     }
 }
