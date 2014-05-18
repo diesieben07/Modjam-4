cac69734530e4291188ad28e4825a0eaf1428b60
Lukas Tenbrink <lukastenbrink@googlemail.com>
2014-05-19 00:49:25 +0200

    Make wannafite drop damaged iron swords

--- src/main/java/mod/badores/ore/Wannafite.java
+++ src/main/java/mod/badores/ore/Wannafite.java
@@ -2,8 +2,12 @@ package mod.badores.ore;
 
 import cpw.mods.fml.relauncher.Side;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 /**
  * @author diesieben07
@@ -12,7 +16,12 @@ public class Wannafite extends AbstractOre {
 
 	private static final DamageSource wannafiteDamage = new DamageSource("badores.wannafite");
 
-	@Override
+    @Override
+    protected void addOreDrops(World world, int x, int y, int z, int meta, int fortune, List<ItemStack> drops) {
+        drops.add(new ItemStack(Items.iron_sword, 1, rand.nextInt(100) + 100));
+    }
+
+    @Override
 	public void onHarvest(EntityPlayer miner, World world, int x, int y, int z, Side side, boolean isIngotBlock) {
 		miner.attackEntityFrom(wannafiteDamage, 4);
 	}
